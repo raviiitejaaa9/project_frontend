52f9ec2f063b816ebcb65861a3a032adacf31b9b
raviiitejaaa9 <63893669+raviiitejaaa9@users.noreply.github.com>
2024-11-10 12:39:12 +0530

    Updates

---
+++ precache-manifest.771cf10e001b3653cd35dc32b5cb7901.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5761287dfd67e97cb8169d79f284ecb",
+    "url": "/project_frontend/index.html"
+  },
+  {
+    "revision": "f91df4a04c677e2be19e",
+    "url": "/project_frontend/static/css/main.a6029b24.chunk.css"
+  },
+  {
+    "revision": "8465890371b9c29f6086",
+    "url": "/project_frontend/static/js/2.89ef5fac.chunk.js"
+  },
+  {
+    "revision": "fac1f3ff5e8ebdfed3580bcec7c38cc3",
+    "url": "/project_frontend/static/js/2.89ef5fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050c804319d27429b00f",
+    "url": "/project_frontend/static/js/3.37afe583.chunk.js"
+  },
+  {
+    "revision": "f91df4a04c677e2be19e",
+    "url": "/project_frontend/static/js/main.9f999341.chunk.js"
+  },
+  {
+    "revision": "2567934414b3c8df2006",
+    "url": "/project_frontend/static/js/runtime-main.9d45c72e.js"
+  },
+  {
+    "revision": "e496d7ccf54886ee88eaab0717c27250",
+    "url": "/project_frontend/static/media/profile-icon-design-free-vector.e496d7cc.jpg"
+  }
+]);
